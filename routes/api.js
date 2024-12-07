d2c5741e482835ae0e2d992dc761d03f60f15740
Santi <canosantiago404@gmail.com>
2024-12-07 10:45:54 -0300

    api routes /api/books completed

--- routes/api.js
+++ routes/api.js
@@ -14,18 +14,50 @@ const Book = require("../models/Book");
 module.exports = function (app) {
   app
     .route("/api/books")
-    .get(function (req, res) {
+    .get(async function (req, res) {
       //response will be array of book objects
       //json res format: [{"_id": bookid, "title": book_title, "commentcount": num_of_comments },...]
+      try {
+        const books = await Book.find({}, "_id title commentcount");
+        res.json(books);
+      } catch (err) {
+        res.status(500).json({ error: "Error retrieving books" });
+      }
     })
 
-    .post(function (req, res) {
+    .post(async function (req, res) {
       let title = req.body.title;
       //response will contain new book object including atleast _id and title
+
+      if (!title) {
+        return res.send("missing required field title");
+      }
+
+      try {
+        const newBook = new Book({
+          title: title,
+          commentcount: 0,
+          comments: [],
+        });
+
+        const savedBook = await newBook.save();
+        res.status(201).json({
+          _id: savedBook._id,
+          title: savedBook.title,
+        });
+      } catch (err) {
+        res.status(500).json({ error: "Error creating book" });
+      }
     })
 
-    .delete(function (req, res) {
+    .delete(async function (req, res) {
       //if successful response will be 'complete delete successful'
+      try {
+        await Book.deleteMany({});
+        res.status(200).send("complete delete successful");
+      } catch (err) {
+        res.status(500).json({ error: "Error deleting all books" });
+      }
     });
 
   app
