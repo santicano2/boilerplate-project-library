b3e65cf8304b2a0ca01bef3ed0b06046612cfab7
Santi <canosantiago404@gmail.com>
2024-12-07 12:11:51 -0300

    completed api/books/:id routes

--- routes/api.js
+++ routes/api.js
@@ -62,19 +62,71 @@ module.exports = function (app) {
 
   app
     .route("/api/books/:id")
-    .get(function (req, res) {
+    .get(async function (req, res) {
       let bookid = req.params.id;
       //json res format: {"_id": bookid, "title": book_title, "comments": [comment,comment,...]}
+      try {
+        const book = await Book.findById(bookid);
+
+        if (!book) {
+          return res.send("no book exists");
+        }
+
+        res.json({
+          _id: book._id,
+          title: book.title,
+          comments: book.comments,
+        });
+      } catch (err) {
+        return res.status(500).json({ error: "Error retrieving book" });
+      }
     })
 
-    .post(function (req, res) {
+    .post(async function (req, res) {
       let bookid = req.params.id;
       let comment = req.body.comment;
       //json res format same as .get
+
+      if (!comment) {
+        return res.send("missing required field comment");
+      }
+
+      try {
+        const book = await Book.findById(bookid);
+
+        if (!book) {
+          return res.send("no book exists");
+        }
+
+        book.comments.push(comment);
+        book.commentcount += 1;
+
+        const updatedBook = await book.save();
+
+        res.json({
+          _id: updatedBook._id,
+          title: updatedBook.title,
+          comments: updatedBook.comments,
+        });
+      } catch (err) {
+        return res.send("no book exists");
+      }
     })
 
-    .delete(function (req, res) {
+    .delete(async function (req, res) {
       let bookid = req.params.id;
       //if successful response will be 'delete successful'
+
+      try {
+        const result = await Book.findByIdAndDelete(bookid);
+
+        if (!result) {
+          return res.send("no book exists");
+        }
+
+        return res.send("delete successful");
+      } catch (err) {
+        return res.status(500).json({ error: "Error deleting book" });
+      }
     });
 };
